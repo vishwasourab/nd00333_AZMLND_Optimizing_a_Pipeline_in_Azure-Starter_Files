39c5eed226b7d5e8556f47a376b4d89c0ad071cf
Vishwa Sourab Brungi <vishwasourab05@gmail.com>
2020-10-03 06:19:14 +0000

    Updates in project files

--- train.py
+++ train.py
@@ -11,28 +11,6 @@ from azureml.core import Dataset
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-web_path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-
-#ds = ### YOUR CODE HERE ###
-ds = Dataset.Tabular.from_delimited_files(path=web_path)
-
-
-
-x, y = clean_data(ds)
-
-
-
-# TODO: Split data into train and test sets.
-
-x_train, x_test, y_traain, y_test = train_test_split(x,y, test_size=0.2)
-
-# ## YOUR CODE HERE ###a
-
-run = Run.get_context()
-
 def clean_data(data):
     # Dict for cleaning data
     months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
@@ -60,6 +38,25 @@ def clean_data(data):
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
     
     return x_df, y_df
+
+
+# TODO: Create TabularDataset using TabularDatasetFactory
+# Data is located at:
+web_path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+
+
+#ds = ### YOUR CODE HERE ###
+ds = Dataset.Tabular.from_delimited_files(path=web_path)
+
+x, y = clean_data(ds)
+
+# TODO: Split data into train and test sets.
+
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2)
+
+# ## YOUR CODE HERE ###a
+
+run = Run.get_context()
 
 
 def main():
